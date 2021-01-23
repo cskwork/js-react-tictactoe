6109756a0f734ffb26b05b65798bd69dd3db894a
Danny <csk917@hotmail.com>
2021-01-23 18:23:52 +0900

    add declareWinner

--- src/index.js
+++ src/index.js
@@ -44,6 +44,12 @@ class Board extends React.Component{
 	//Handle square state in parent Board Component
 	handleClick(i){
 		const squares = this.state.squares.slice();
+		
+		//Ignore if square is filled or already won.
+		if(calculateWinner(squares) || squares[i] ){
+			return;
+		}
+
 		squares[i] = this.state.xIsNext ? 'X':'O';
 		this.setState ({ squares: squares,
 						 xIsNext: !this.state.xIsNext,
@@ -58,7 +64,17 @@ class Board extends React.Component{
 	}
 
 	render(){
-		const status='Next player: X';
+		const winner = calculateWinner(this.state.squares);
+		//const status='Next player: X' ;
+		//Switch player dep. on state
+		//const status = '다음 선수: '+ (this.state.xIsNext ? 'X' : 'O');
+		let status;
+
+		if(winner){
+			status = '승리 : '+winner;
+		}else{
+			status = '다음 선수: '+ (this.state.xIsNext ? 'X' : 'O');
+		}
 		
 		return(
 			<div>
@@ -106,3 +122,25 @@ ReactDOM.render(
 				<Game />,
 				document.getElementById('root')
 				);
+
+
+//Helper function to declare winnder
+function calculateWinner(squares){
+	const lines =[
+	[0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+	];
+	for (let i=0; i<lines.length; i++){
+		const [a,b,c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				return squares[a];
+		}
+	}
+	return null;
+}
